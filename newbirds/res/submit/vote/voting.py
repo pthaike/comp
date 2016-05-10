c9200e03bec92ce3bd190f59ad81bb0464547c9c
pthaike <pthaike@gmail.com>
2016-05-10 19:27:00 +0800

    5.10
    voting 14 days and xgb for 0.8 and 0.2

--- newbirds/res/submit/vote/voting.py
+++ newbirds/res/submit/vote/voting.py
@@ -6,10 +6,10 @@ import pandas as pd
 import time
 
 if __name__ == '__main__':
-	f1 = '20160508133848xgb_mutil_have28_14_14.csv'
-	f2 = '20160504103829xgb_mutil_14_14.csv'
+	f1 = '20160509175957xgb_mutil_have28_14_14.csv'
+	f2 = '20160509192835xgb_mutil_have28_14_14.csv'
 	df1 = pd.read_csv(f1,header = None, names= ['id', 'type', 'pred'])
 	df2 = pd.read_csv(f2,header = None, names= ['id', 'type', 'pred'])
-	df1.pred = df1.pred * 0.6+ df2.pred * 0.4
+	df1.pred = df1.pred * 0.8+ df2.pred * 0.2
 
-	df1.to_csv('submit.csv',header=False, index = False)
+	df1.to_csv('050917_050919_submit.csv',header=False, index = False)
